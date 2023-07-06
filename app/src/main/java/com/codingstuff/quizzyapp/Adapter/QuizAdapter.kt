a4e40daf2fefc82432864c930da875dc46a97faa
ZarAfshan <zarafshan.codeninja@gmail.com>
2023-07-06 12:07:18 +0500

    Quiz adapter set to take quiz

--- app/src/main/java/com/codingstuff/quizzyapp/Adapter/QuizAdapter.kt
+++ app/src/main/java/com/codingstuff/quizzyapp/Adapter/QuizAdapter.kt
@@ -4,12 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.codingstuff.quizzyapp.Model.QuizModel
 import com.codingstuff.quizzyapp.R
 
 class QuizAdapter(private val quizList: List<QuizModel>) : RecyclerView.Adapter<QuizAdapter.QuizViewHolder>() {
+    private var isExplanationVisible = false
 
     inner class QuizViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         // Initialize views in the item layout
@@ -20,7 +22,7 @@ class QuizAdapter(private val quizList: List<QuizModel>) : RecyclerView.Adapter<
         val tv_op_a_ans: TextView = itemView.findViewById(R.id.tv_correct_ans_a)
 
         val tv_op_b: TextView = itemView.findViewById(R.id.tv_op_b)
-        val tv_op_ans: TextView = itemView.findViewById(R.id.tv_correct_ans_b)
+        val tv_op_b_ans: TextView = itemView.findViewById(R.id.tv_correct_ans_b)
         val img_res_op_b: ImageView =itemView.findViewById(R.id.img_res_op_b)
 
         val tv_op_c: TextView = itemView.findViewById(R.id.tv_op_c)
@@ -31,7 +33,17 @@ class QuizAdapter(private val quizList: List<QuizModel>) : RecyclerView.Adapter<
         val tv_op_d_ans: TextView = itemView.findViewById(R.id.tv_correct_ans_d)
         val img_res_op_d: ImageView =itemView.findViewById(R.id.img_res_op_d)
 
-       // val answerTextView: TextView = itemView.findViewById(R.id.answerTextView)
+        val llOpDExpShow_a: LinearLayout = itemView.findViewById(R.id.ll_op_a_exp_show)
+        val llOpDExpShow_b: LinearLayout = itemView.findViewById(R.id.ll_op_b_exp_show)
+        val llOpDExpShow_c: LinearLayout = itemView.findViewById(R.id.ll_op_c_exp_show)
+        val llOpDExpShow_d: LinearLayout = itemView.findViewById(R.id.ll_op_d_exp_show)
+
+        val ll_main_op_a: LinearLayout = itemView.findViewById(R.id.ll_main_op_a)
+        val ll_main_op_b: LinearLayout = itemView.findViewById(R.id.ll_main_op_b)
+        val ll_main_op_c: LinearLayout = itemView.findViewById(R.id.ll_main_op_c)
+        val ll_main_op_d: LinearLayout = itemView.findViewById(R.id.ll_main_op_d)
+
+        // val answerTextView: TextView = itemView.findViewById(R.id.answerTextView)
         // ... Add more views as needed
     }
 
@@ -52,11 +64,250 @@ class QuizAdapter(private val quizList: List<QuizModel>) : RecyclerView.Adapter<
         holder.tv_op_c.text = quiz.option_c
         holder.tv_op_d.text = quiz.option_d
 
-        // ... Bind more data to other views as needed
+
+        val corr_opt = quiz.answer
+        val reason = quiz.reason
+        val ans = "$corr_opt\n+$reason"
+
+        holder.ll_main_op_a.setOnClickListener(View.OnClickListener {
+            if (corr_opt=="option_a"){
+                holder.img_res_op_a.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.VISIBLE
+                holder.tv_op_a_ans.text = "$corr_opt\n$reason"
+                holder.tv_op_a_ans.visibility = View.VISIBLE
+
+            }else{
+                holder.img_res_op_a.visibility = View.VISIBLE
+                holder.img_res_op_a.setImageResource(R.drawable.close)
+            }
+            freezTheResult(holder)
+
+        })
+        holder.ll_main_op_b.setOnClickListener(View.OnClickListener {
+            if (corr_opt =="option_b"){
+                holder.llOpDExpShow_b.visibility = View.VISIBLE
+                holder.img_res_op_b.setImageResource(R.drawable.right_mark)
+                holder.tv_op_b_ans.text = "$corr_opt\n$reason"
+                holder.tv_op_b_ans.visibility = View.VISIBLE
+
+
+            }else{
+                holder.img_res_op_b.visibility = View.VISIBLE
+                holder.img_res_op_b.setImageResource(R.drawable.close)
+            }
+            freezTheResult(holder)
+
+        })
+        holder.ll_main_op_c.setOnClickListener(View.OnClickListener {
+            if (corr_opt =="option_c"){
+                holder.llOpDExpShow_c.visibility = View.VISIBLE
+                holder.img_res_op_c.setImageResource(R.drawable.right_mark)
+                holder.tv_op_c_ans.text = "$corr_opt\n$reason"
+                holder.tv_op_c_ans.visibility = View.VISIBLE
+
+
+            }else{
+                holder.img_res_op_c.visibility = View.VISIBLE
+                holder.img_res_op_c.setImageResource(R.drawable.close)
+            }
+            freezTheResult(holder)
+
+        })
+        holder.ll_main_op_d.setOnClickListener(View.OnClickListener {
+            if (corr_opt =="option_d"){
+                holder.img_res_op_d.visibility = View.VISIBLE
+                holder.llOpDExpShow_d.visibility = View.VISIBLE
+                holder.img_res_op_d.setImageResource(R.drawable.right_mark)
+                holder.tv_op_d_ans.text = "$corr_opt\n$reason"
+
+            }else{
+                holder.img_res_op_d.visibility = View.VISIBLE
+                holder.img_res_op_d.setImageResource(R.drawable.close)
+            }
+            freezTheResult(holder)
+        })
+
+
+
+        holder.llOpDExpShow_a.setOnClickListener(View.OnClickListener {
+            toggleExplanation(holder.llOpDExpShow_a,holder.tv_op_a_ans)
+        })
+        holder.llOpDExpShow_b.setOnClickListener(View.OnClickListener {
+            toggleExplanation(holder.llOpDExpShow_b,holder.tv_op_b_ans)
+        })
+        holder.llOpDExpShow_c.setOnClickListener(View.OnClickListener {
+            toggleExplanation(holder.llOpDExpShow_c,holder.tv_op_c_ans)
+        })
+        holder.llOpDExpShow_d.setOnClickListener(View.OnClickListener {
+            toggleExplanation(holder.llOpDExpShow_d,holder.tv_op_d_ans)
+        })
     }
 
     override fun getItemCount(): Int {
         // Return the size of the dataset
         return quizList.size
     }
+
+    private fun toggleExplanation(res_layout : LinearLayout, tv_corr_ans: TextView) {
+
+        val textView: TextView = res_layout.getChildAt(0) as TextView
+        val imageView: ImageView = res_layout.getChildAt(1) as ImageView
+
+        if (isExplanationVisible) {
+            // Hide explanation
+           textView.text = "Show Explanation"
+            imageView.setImageResource(R.drawable.drop_down_arrow)
+            tv_corr_ans.visibility = View.GONE
+        } else if (isExplanationVisible==false){
+            // Show explanation
+            textView.text = "Hide Explanation"
+            imageView.setImageResource(R.drawable.close_menu_arrow_up)
+            tv_corr_ans.visibility = View.VISIBLE
+
+        }
+
+        isExplanationVisible = !isExplanationVisible
+    }
+    fun freezTheResult(holder: QuizViewHolder){
+        holder.ll_main_op_a.isClickable = false
+        holder.ll_main_op_b.isClickable = false
+        holder.ll_main_op_c.isClickable = false
+        holder.ll_main_op_d.isClickable = false
+
+    }
+    fun showRightOptn(holder: QuizViewHolder, corr_opt:String, ans:String){
+
+        if (corr_opt=="option_a"){
+            holder.img_res_op_a.setImageResource(R.drawable.right_mark)
+            holder.llOpDExpShow_a.visibility = View.VISIBLE
+            holder.tv_op_a_ans.text = ans
+            holder.tv_op_a_ans.visibility = View.VISIBLE
+
+        }else{
+            holder.img_res_op_a.visibility = View.VISIBLE
+            holder.img_res_op_a.setImageResource(R.drawable.close)
+        }
+        freezTheResult(holder)
+        freezTheResult(holder)
+        if (corr_opt =="option_b"){
+            holder.llOpDExpShow_b.visibility = View.VISIBLE
+            holder.img_res_op_b.setImageResource(R.drawable.right_mark)
+            holder.tv_op_b_ans.text = ans
+            holder.tv_op_b_ans.visibility = View.VISIBLE
+
+
+        }else{
+            holder.img_res_op_b.visibility = View.VISIBLE
+            holder.img_res_op_b.setImageResource(R.drawable.close)
+        }
+        freezTheResult(holder)
+        if (corr_opt =="option_c"){
+            holder.llOpDExpShow_c.visibility = View.VISIBLE
+            holder.img_res_op_c.setImageResource(R.drawable.right_mark)
+            holder.tv_op_c_ans.text = ans
+            holder.tv_op_c_ans.visibility = View.VISIBLE
+
+
+        }else{
+            holder.img_res_op_c.visibility = View.VISIBLE
+            holder.img_res_op_c.setImageResource(R.drawable.close)
+        }
+        freezTheResult(holder)
+        if (corr_opt =="option_d"){
+            holder.img_res_op_d.visibility = View.VISIBLE
+            holder.llOpDExpShow_d.visibility = View.VISIBLE
+            holder.img_res_op_d.setImageResource(R.drawable.right_mark)
+            holder.tv_op_d_ans.text = ans
+
+        }else{
+            holder.img_res_op_d.visibility = View.VISIBLE
+            holder.img_res_op_d.setImageResource(R.drawable.close)
+        }
+        freezTheResult(holder)
+
+    }
+    fun showWrongOptn(holder: QuizViewHolder, corr_opt:String?, ans:String){
+
+        when (corr_opt){
+            "option_a" ->{
+                holder.img_res_op_a.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.VISIBLE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+
+            }
+            "option_b" ->{
+                holder.llOpDExpShow_b.visibility = View.VISIBLE
+                holder.img_res_op_b.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+            }
+            "option_c" ->{
+                holder.llOpDExpShow_c.visibility = View.VISIBLE
+                holder.img_res_op_c.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+
+            }
+            "option_d" ->{
+                holder.img_res_op_d.visibility = View.VISIBLE
+                holder.llOpDExpShow_d.visibility = View.VISIBLE
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+
+            }
+        }
+
+        freezTheResult(holder)
+
+    }
+    fun showRightOptn(holder: QuizViewHolder, corr_opt:String?, ans:String){
+
+        when (corr_opt){
+            "option_a" ->{
+                holder.img_res_op_a.setImageResource(R.drawable.right_mark)
+                holder.img_res_op_a.visibility = View.VISIBLE
+                holder.llOpDExpShow_a.visibility = View.VISIBLE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+
+            }
+            "option_b" ->{
+                holder.llOpDExpShow_b.visibility = View.VISIBLE
+                holder.img_res_op_b.visibility = View.VISIBLE
+                holder.img_res_op_b.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+            }
+            "option_c" ->{
+                holder.llOpDExpShow_c.visibility = View.VISIBLE
+                holder.img_res_op_c.visibility = View.VISIBLE
+
+                holder.img_res_op_c.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_d.visibility = View.GONE
+
+            }
+            "option_d" ->{
+                holder.img_res_op_d.visibility = View.VISIBLE
+                holder.img_res_op_d.setImageResource(R.drawable.right_mark)
+                holder.llOpDExpShow_d.visibility = View.VISIBLE
+                holder.llOpDExpShow_a.visibility = View.GONE
+                holder.llOpDExpShow_b.visibility = View.GONE
+                holder.llOpDExpShow_c.visibility = View.GONE
+
+            }
+        }
+
+        freezTheResult(holder)
+
+    }
+
 }
